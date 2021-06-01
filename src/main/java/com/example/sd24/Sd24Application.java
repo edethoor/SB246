8614e38798e6ca5db5b69ed92d602dc37d2bac35
[a556724] etienne dethoor <etienne.dethoor@atos.net>
2021-06-01 09:55:00 +0200

    Init App from spring Initializr

---
+++ src/main/java/com/example/sd24/Sd24Application.java
@@ -0,0 +1,13 @@
+package com.example.sd24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sd24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sd24Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sd24/Sd24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sd24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sd24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
